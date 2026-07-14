e151d55bdf1559265eeccd169969dc4ad815ca5b
Charu-sarswat <charusaraswat639649@gmail.com>
2026-07-14 10:35:14 +0530

    ES module

--- react-app/netlify/functions/send-email.js
+++ react-app/netlify/functions/send-email.js
@@ -1,4 +1,4 @@
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   // Enable CORS for frontend requests
   const headers = {
     'Access-Control-Allow-Origin': '*',
